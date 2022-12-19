99eb19fd66b60fc656b8acab9eff616c302fcda0
Zhalynbbekov <101692824+Zhalynbbekov@users.noreply.github.com>
2022-12-19 14:33:59 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,18 +5,36 @@ public class Main {
         Director director = new Director("Umut","123");
         Delivery delivery = new Delivery("Ilyaz", "1234");
         Zavkhoz zavkhoz = new Zavkhoz("Zhantai", "12345");
-        System.out.println("Enter a type of your account: ");
-        String type = scan.nextLine();
-        if(type.equals("director")){
-            director.login();
-        }
-        else if(type.equals("delivery")){
-            delivery.login();
-        }
-        else if(type.equals("zavkhoz")){
-            zavkhoz.login();
-        }else{
-            System.out.println("We can't find that kind of account.\n Try again");
+        System.out.println("Log in or register?");
+        String loginOrRegister = scan.nextLine();
+        if(loginOrRegister.equals("login")){
+            System.out.println("Enter a type of your account: ");
+            String type = scan.nextLine();
+            if(type.equals("director")){
+                director.login();
+            }
+            else if(type.equals("delivery")){
+                delivery.login();
+            }
+            else if(type.equals("zavkhoz")){
+                zavkhoz.login();
+            }else{
+                System.out.println("We can't find that kind of account.\n Try again");
+            }
+        }else if(loginOrRegister.equals("register")){
+            System.out.println("Enter a type of your account: ");
+            String registerType = scan.nextLine();
+            if(registerType.equals("zavkhoz")){
+                zavkhoz.register();
+            }
+            else if(registerType.equals("director")){
+                director.register();
+            }
+            else if(registerType.equals("delivery")){
+                delivery.register();
+            }else{
+                System.out.println("We can't find that kind of account.\n Try again");
+            }
         }
     }
 }
